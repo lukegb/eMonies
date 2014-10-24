5fca16ae72ade430be2550d8728c408787375d67
Ali Lown <ali@lown.me.uk>
2014-10-25 00:13:28 +0100

    Also fixup minimal_transactions to use amountmultiplier

--- app/models/minimal_transaction.rb
+++ app/models/minimal_transaction.rb
@@ -1,4 +1,8 @@
+require 'AmountMultiplier'
+
 class MinimalTransaction < ActiveRecord::Base
   belongs_to :from_person, class_name: :Person
   belongs_to :to_person, class_name: :Person
+
+  include AmountMultiplier
 end
