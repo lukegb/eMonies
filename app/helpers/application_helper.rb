3d824979afa23af01cf51bb5c20b95dc0832bdf1
Luke Granger-Brown <git@lukegb.com>
2014-10-30 03:15:10 +0000

    Adds redcarpet and markdown renders purchase descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def to_money(i)
     number_to_currency(i, unit: "£", negative_format: "(%u%n)")
   end
+
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true, tables: true)
+    @markdown.render(content).html_safe
+  end
 end
